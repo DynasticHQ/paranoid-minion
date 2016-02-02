d5dfafaad245f3055445a226c067687669da07a7
Miguel Moll <miguel.moll@gmail.com>
2016-02-01 22:43:24 -0500

    Added new line after the license

--- minion.go
+++ minion.go
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with The Paranoid Minion.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package main
 
 import "fmt"
