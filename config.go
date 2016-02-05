e139531d00de64af09d14f1d62a9d24916159d88
Miguel Moll <miguel.moll@gmail.com>
2016-02-04 22:57:33 -0500

    Added basic logging and config features

---
+++ config.go
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2016 Miguel Moll
+ *
+ * This file is part of the Paranoid Minion
+ *
+ * The Paranoid Minion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Paranoid Minion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Paranoid Minion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package minion
+
+import "github.com/BurntSushi/toml"
+
+// MinionConfig describes the config for the minion.
+type MinionConfig struct {
+	Log LogConfig
+}
+
+// Log describes the log group in a TOML format
+type LogConfig struct {
+	Level  string
+	Type   string
+	Format string
+}
+
+// MinionConfig constatns
+const (
+	LogLevelInfo    = "info"
+	LogLevelDebug   = "debug"
+	LogLevelWarning = "warning"
+	LogLevelError   = "error"
+
+	LogFormatJSON = "json"
+	LogFormatText = "text"
+
+	LogTypeStdout = "stdout"
+	LogTypeFile   = "file"
+)
+
+// InitConfig initalizes the MinionConfig by reading the path config.
+// Returns pointer to a MinionConfig if everything loads fine.
+// nil and an error otherwise.
+func InitConfig(path string) (*MinionConfig, error) {
+
+	mc := NewConfig()
+	err := LoadConfigFile(path, &mc)
+	if err != nil {
+		return &mc, err
+	}
+
+	return &mc, nil
+}
+
+// LoadConfig takes a string path to load a TOML file and saves the data in mc.
+// It returns an error if something went wrong.
+func LoadConfigFile(path string, mc *MinionConfig) error {
+
+	_, err := toml.DecodeFile(path, mc)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// NewConfig returns a MinionConfig with config defaults for the minion.
+func NewConfig() MinionConfig {
+
+	mc := MinionConfig{}
+
+	mc.Log.Level = LogLevelInfo
+	mc.Log.Type = LogTypeFile
+	mc.Log.Format = LogFormatText
+
+	return mc
+}
---
+++ logging.go
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2016 Miguel Moll
+ *
+ * This file is part of the Paranoid Minion
+ *
+ * The Paranoid Minion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Paranoid Minion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Paranoid Minion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package minion
+
+import "github.com/Sirupsen/logrus"
+
+type Logger struct {
+	log *logrus.Logger
+}
+
+type LogFields map[string]interface{}
+
+var Log *Logger
+
+func InitLogging(mc MinionConfig) {
+	Log = &Logger{logrus.New()}
+	Log.SetLogFormatter(mc.Log.Format)
+	Log.SetLogLevel(mc.Log.Level)
+	// TODO(miguelmoll): Implement a type for logger. e.g. to file or stdout
+	// Set to stdout because of defaults.
+
+}
+
+func (l *Logger) SetLogFormatter(format string) {
+	switch format {
+	case LogFormatJSON:
+		l.log.Formatter = new(logrus.JSONFormatter)
+	default:
+		l.log.Formatter = &logrus.TextFormatter{FullTimestamp: true}
+	}
+}
+
+func (l *Logger) SetLogLevel(level string) {
+	switch level {
+	case LogLevelDebug:
+		l.log.Level = logrus.DebugLevel
+	case LogLevelWarning:
+		l.log.Level = logrus.WarnLevel
+	case LogLevelError:
+		l.log.Level = logrus.ErrorLevel
+	default:
+		l.log.Level = logrus.InfoLevel
+	}
+}
+
+// Debug logs a message at level Debug.
+func (l *Logger) Debug(args ...interface{}) {
+	l.log.Debug(args...)
+}
+
+// Debugf logs a message at level Debug.
+func (l *Logger) Debugf(format string, args ...interface{}) {
+	l.log.Debugf(format, args...)
+}
+
+// Info logs a message at level Info.
+func (l *Logger) Info(args ...interface{}) {
+	l.log.Info(args...)
+}
+
+// Infof logs a message at level Info.
+func (l *Logger) Infof(format string, args ...interface{}) {
+	l.log.Infof(format, args...)
+}
+
+// Warn logs a message at level Warn.
+func (l *Logger) Warn(args ...interface{}) {
+	l.log.Warn(args...)
+}
+
+// Warnf logs a message at level Warn.
+func (l *Logger) Warnf(format string, args ...interface{}) {
+	l.log.Warnf(format, args...)
+}
+
+// Error logs a message at level Error.
+func (l *Logger) Error(args ...interface{}) {
+	l.log.Error(args...)
+}
+
+// Errorf logs a message at level Error.
+func (l *Logger) Errorf(format string, args ...interface{}) {
+	l.log.Errorf(format, args...)
+}
