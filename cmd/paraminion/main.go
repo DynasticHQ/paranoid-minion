fd48c1b78e13fab288633b2f46a93e3b57c9a8bd
Miguel Moll <miguel.moll@gmail.com>
2016-02-07 23:31:38 -0500

    Happy with a default main.go initial layout

--- cmd/paraminion/main.go
+++ cmd/paraminion/main.go
@@ -20,19 +20,65 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"dynastic.ninja/paranoid/minion"
+	"dynastic.ninja/paranoid/minion/internal"
+	"dynastic.ninja/paranoid/minion/msgqueue"
+	"dynastic.ninja/paranoid/minion/transaction"
+)
+
+var (
+	configPath  = flag.String("config", "minion.toml", "TOML file used by the Minion.")
+	showVersion = flag.Bool("version", false, "print version string")
 )
 
 func main() {
 
-	config, err := minion.InitConfig("minion.toml")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf(version.String("Paranoid Minion"))
+		return
+	}
+
+	err := minion.InitConfig(*configPath)
 	if err != nil {
 		fmt.Println("Unable to load config file. Error:", err)
 		fmt.Println("Using minion defaults.")
 	}
-	minion.InitLogging(*config)
+
+	minion.InitLogging(minion.Config)
+
+	incomingMsg := make(chan msgqueue.QueueData)
+	outgoingMsg := make(chan msgqueue.QueueData)
+
+	supervisor := &transaction.Supervisor{}
+	supervisor.IncomingChannel = incomingMsg
+	supervisor.OutgoingChannel = outgoingMsg
+
+	msgQueue := &msgqueue.NsqDriver{}
+	msgQueue.SetIncomingChannel(incomingMsg)
+	msgQueue.Initialize("127.0.0.1:4161", "agent_id")
+	msgQueue.Connect()
 
 	minion.Log.Info("Minion has started. Ready Up!")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go supervisor.Run()
+
+	for {
+		select {
+		case <-sigChan:
+			minion.Log.Info("Minion is stopping.")
+			msgQueue.Shutdown()
+			return
+		}
+	}
 }
